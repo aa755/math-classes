04c00bb4650f2e78d9d8299756755607d42cc388
msozeau <msozeau@0cf17b13-060f-0410-b1b1-c666bec9822a>
2012-12-08 21:42:16 +0000

    Fix some hints that should not be general but instead instantiated at the moment of declaration
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/coq-contribs/trunk@2155 0cf17b13-060f-0410-b1b1-c666bec9822a

--- implementations/stdlib_binary_integers.v
+++ implementations/stdlib_binary_integers.v
@@ -53,12 +53,12 @@ Proof.
 Qed.
 
 (* SRpair N and Z are isomorphic *)
-Definition Npair_to_Z (x : SRpair N) : Z := 'pos x - 'neg x.
+Definition Npair_to_Z (x : SRpair N) := 'pos x + - 'neg x.
 
 Instance: Proper (=) Npair_to_Z.
 Proof.
   intros x y E. do 2 red in E. unfold Npair_to_Z.
-  apply (right_cancellation (+) ('neg y + 'neg x)). ring_simplify.
+  apply (right_cancellation (+) ('neg y + 'neg x)); ring_simplify.
   now rewrite <-?rings.preserves_plus, E, commutativity.
 Qed.
 
